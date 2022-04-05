41903caa14be2044134dc70452cd07c6416a400f
dingding-21 <phj8221@naver.com>
2022-04-06 06:38:30 +0900

    [ADD] order related entity

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,28 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.*;
+
+@Entity
+@Getter
+@Setter
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING) //꼭 STRING으로 써야됨!, 순서 안밀림
+    private DeliveryStatus status; //READY, COMP
+}
